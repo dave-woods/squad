149dcfdcd244a9c0c9ce26eb0e205903ffd492c4
Dave Woods <dwoodscs@gmail.com>
2019-11-03 19:40:01 +0000

    Added nav link to home page

--- components/nav.js
+++ components/nav.js
@@ -1,31 +1,31 @@
 import React from 'react'
 import Link from 'next/link'
 
-const links = [
-  { href: 'https://github.com/segmentio/create-next-app', label: 'Github' }
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`
-  return link
-})
+// const links = [
+//   { href: '/', label: 'Home' }
+// ].map(link => {
+//   link.key = `nav-link-${link.label}`
+//   return link
+// })
 
 const Nav = () => (
   <nav>
-    {/* <ul>
+    <ul>
       <li>
         <Link prefetch href="/">
           <a>Home</a>
         </Link>
       </li>
       <ul>
-        {links.map(({ key, href, label }) => (
+        {/* {links.map(({ key, href, label }) => (
           <li key={key}>
             <Link href={href}>
               <a>{label}</a>
             </Link>
           </li>
-        ))}
+        ))} */}
       </ul>
-    </ul> */}
+    </ul>
 
     <style jsx>{`
       :global(body) {
